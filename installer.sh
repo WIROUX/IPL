7ddba80f4ad24999cb9d96bf76c27788dfd71e80
SaDWX <93474444+WIROUX@users.noreply.github.com>
2022-06-08 14:53:55 +0430

    Update installer.sh

--- installer.sh
+++ installer.sh
@@ -1,21 +1,8 @@
-#!/bin/bash
-
-#color
-#============
-g='\033[92m'
-#===========
-clear
-apt update && apt upgrade -y
-clear
-pkg install python -y 
-clear
 pip install --upgrade pip
 pip install colorama 
-pip install requests
-clear
-#================================================
+pip install requests 
 #starting IPL tool
-echo -e "${g}starting IPL tool ....."
+echo "starting IPL tool ....."
 sleep 5
 pyhon IPL.py 
 
