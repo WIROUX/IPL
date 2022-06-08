31b45d85ccaa86dd0df63a93f29ce8677ee8ad42
SaDWX <93474444+WIROUX@users.noreply.github.com>
2022-06-08 15:33:24 +0430

    ins.txt

--- installer.sh
+++ installer.sh
@@ -1,9 +1,6 @@
-pip install --upgrade pip
-pip install colorama 
-pip install requests 
-#starting IPL tool
-echo "starting IPL tool ....."
-sleep 5
-python IPL.py 
+--upgrade pip
+requests 
+colorama
+
 
 
